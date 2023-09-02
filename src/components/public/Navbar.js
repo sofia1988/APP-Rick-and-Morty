155275488215f26b713ee3fb8f804e9769dc57d0
brunila <brunila110@gmail.com>
2023-09-02 20:55:16 -0300

    arreglo

--- src/components/public/Navbar.js
+++ src/components/public/Navbar.js
@@ -23,35 +23,41 @@ const Navbar = () => {
     }, []);
 
     return (
-
-        <header className='container' >
-            <nav className="navbar navbar-expand-lg  d-flex justify-content-end">
-                <div className="container-fluid">
-                    <div>
-                        <a className="navbar-brand " href="#">
+        <div className='container'>
+            <div className='row'>
+                <div className='container col'>
+                    <div className='col-2 pt-3'>
+                        <a className="navbar-brand" href="#">
                             <img src={logo} width="250" height="150" alt="logo" />
                         </a>
-                    </div> 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active text-white" aria-current="page" href="/.">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="/episode">Episodios</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-white" href="/user-form">Formulario</a>
-                            </li>
-                        </ul>
                     </div>
                 </div>
-            </nav>
-        </header>
+                <header className='col pt-5 d-flex justify-content-end' >
+                    <nav className=" navbar navbar-expand-lg  ">
+                        <div className="container-fluid">
 
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarNav">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <a className="nav-link active text-white" aria-current="page" href="/.">Home</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link text-white" href="/episode">Episodios</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link text-white" href="/user-form">Formulario</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+
+            </div>
+        </div>
     )
 }
 
